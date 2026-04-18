e97b20129629c11aab8e467c65619b354cdf88ea
pavlorudak <112572097+pavlorudak@users.noreply.github.com>
2026-04-18 19:53:58 +0200

    Add email-validator

--- app/auth.py
+++ app/auth.py
@@ -6,7 +6,7 @@ SECRET_KEY = "supersecretkeychange"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(schemes=["pbkdf2_sha256"], deprecated="auto")
 
 
 def hash_password(password):
