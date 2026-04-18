6104f61f6b12f6b846540b8eabd1b28f2d998417
pavlorudak <112572097+pavlorudak@users.noreply.github.com>
2026-04-18 20:00:31 +0200

    Add email-validator

--- app/auth.py
+++ app/auth.py
@@ -4,21 +4,19 @@ from passlib.context import CryptContext
 
 SECRET_KEY = "supersecretkeychange"
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
-pwd_context = CryptContext(schemes=["pbkdf2_sha256"], deprecated="auto")
-
+pwd_context = CryptContext(
+    schemes=["pbkdf2_sha256"],
+    deprecated="auto"
+)
 
 def hash_password(password):
     return pwd_context.hash(password)
 
-
 def verify_password(plain, hashed):
     return pwd_context.verify(plain, hashed)
 
-
 def create_access_token(data: dict):
-    to_encode = data.copy()
     expire = datetime.utcnow() + timedelta(minutes=60)
-    to_encode.update({"exp": expire})
-    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    data.update({"exp": expire})
+    return jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
