7eaddef937950adbbae61238960fb4b4c2e500d2
pavlorudak <112572097+pavlorudak@users.noreply.github.com>
2026-04-18 18:16:46 +0200

    Import server project

---
+++ app/models.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    password = Column(String)
+
+class Company(Base):
+    __tablename__ = "companies"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    name = Column(String)
+    logo = Column(String, nullable=True)
+    address = Column(String)
