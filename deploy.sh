a732de6ffc2ff2349a04164d709973112de9ec97
pavlorudak <112572097+pavlorudak@users.noreply.github.com>
2026-04-18 19:01:02 +0200

    update

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+echo "📤 Subiendo a GitHub..."
 git push
 
+echo "🚀 Desplegando en servidor..."
 ssh -i ~/facturas_new pavlo@192.168.1.231 "
 cd ~/server/webstack &&
-git pull &&
+git pull origin main &&
 docker-compose up -d --build
 "
+
+echo "✅ Deploy terminado"
 
